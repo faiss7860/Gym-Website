1b263aa7096e74de81899eca4a210225ddbab384
Shaikh Fais <faiss7860@gmail.com>
2022-12-04 00:56:24 -0800

    done

--- src/Gym/Components/Home/Navbar/Navbar.jsx
+++ src/Gym/Components/Home/Navbar/Navbar.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import { Link } from "react-router-dom";
-// import '../Navbar/Navbar.css'
-
-// const Navbar = () => {
-//     // const [open, setopen] = useState(false);
-//   return (
-
-//     <div>
-//     <nav className="navbar">
-
-//         <div className="logo">
-//             <h1>Muscle<span>Factory</span></h1>
-//         </div>
-
-//         <ul className="nav-links">
-
-//             <input type="checkbox" id="checkbox_toggle" />
-//             <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
-
-//             <div className="menu">
-//                 <li><Link className='lii' href="" to='/'>Home</Link></li>
-//                 <li><Link className='lii' href="" to='/Schedule'>Schedule</Link></li>
-//                 <li><Link className='lii' href="" to='/Exercises'><span>Free</span> WorkOut</Link></li>
-//                 <li><Link className='lii' href="" to='/MemberShip'>Join <span>Now</span></Link></li>
-//             </div>
-//         </ul>
-//     </nav>
-// </div>
-//   )
-// }
-
-// export default Navbar
-
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../Navbar/Navbar.css'
